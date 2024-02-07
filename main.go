d2778c75cad405655e4ea8d3d2ec2c2e0fd76226
Elton Minetto <elton.minetto@picpay.com>
2024-02-07 09:41:10 -0300

    feat: add webhook

--- main.go
+++ main.go
@@ -85,6 +85,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Application")
 		os.Exit(1)
 	}
+	if err = (&minettodevv1alpha1.Application{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Application")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
